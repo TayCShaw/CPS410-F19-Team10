d2ee85081fae85ae2ab8cb655c60b75fe2bd72dc
TayCShaw <47929530+TayCShaw@users.noreply.github.com>
2019-10-30 15:58:08 -0400

    fgh

--- CPS410Final/Database.cs
+++ CPS410Final/Database.cs
@@ -8,10 +8,13 @@ namespace CPS410Final
 {
     public class Database
     {
-        static SqlConnection connection;
+        static string connectionstring = connectionBuilder().ConnectionString;
+
         static SqlConnectionStringBuilder builder;
 
-        static void openDB(String[] args)
+
+        /************** DATABASE OPERATIONS **************/
+        static void openDB()
         {
             connection.Open();
         }
@@ -21,13 +24,68 @@ namespace CPS410Final
             connection.Close();
         }
 
-        static void connectionBuilder()
+        static void query()
+        {
+
+        }
+
+        static void addNewUser()
+        {
+
+        }
+
+        static void checkCredentials(String username, String password)
+        {
+            openDB();
+            SqlConnection connect = new SqlConnection(connectionstring);
+            SqlCommand search = new SqlCommand("SELECT * FROM Users WHERE userName = @username", connect);
+
+            search.Parameters.AddWithValue("@username", username);
+            try
+            {
+                connect.Open();
+                SqlDataReader reader = search.ExecuteReader();
+                reader.Read();
+
+                if (!reader.HasRows)
+                {
+                    
+                }
+                else
+                {
+                    pass = Security.Sha256(password);
+                }
+            }
+            catch (Exception er)
+            {
+                lblErrorMessages.Text = er.ToString();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        search.ExecuteReader();
+        }
+
+        /************** END DATABASE OPERATIONS **************/
+
+        static SqlConnectionStringBuilder connectionBuilder()
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.ConnectionString = "Data Source = tutorsite.database.windows.net; Initial Catalog = siteDB; User ID = team10; " +
+                "Password = ********; Connect Timeout = 30; Encrypt = True; TrustServerCertificate = False; ApplicationIntent = ReadWrite;" +
+                " MultiSubnetFailover = False";
             builder.DataSource = "tutorsite.database.windows.net";
             builder.UserID = "team10";
             builder.Password = "Cps410Cps410";
             builder.InitialCatalog = "siteDB";
+
+            return builder;
+
         }
     }
 }
+ 
+ 
