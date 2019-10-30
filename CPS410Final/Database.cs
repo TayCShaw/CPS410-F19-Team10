c7e2a3a796db29b1fef05ce2466eb4e1ee2d2084
TayCShaw <47929530+TayCShaw@users.noreply.github.com>
2019-10-30 15:11:58 -0400

    Added base functionality to access database

---
+++ CPS410Final/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CPS410Final
+{
+    public class Database
+    {
+        static SqlConnection connection;
+        static SqlConnectionStringBuilder builder;
+
+        static void openDB(String[] args)
+        {
+            connection.Open();
+        }
+
+        static void closeDB()
+        {
+            connection.Close();
+        }
+
+        static void connectionBuilder()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "tutorsite.database.windows.net";
+            builder.UserID = "team10";
+            builder.Password = "Cps410Cps410";
+            builder.InitialCatalog = "siteDB";
+        }
+    }
+}
