faeee4a61b2a5da420b1162995a168b7aa52a8be
TayCShaw <47929530+TayCShaw@users.noreply.github.com>
2019-11-04 14:07:14 -0500

    Added more database functionality.

--- CPS410Final/Database.cs
+++ CPS410Final/Database.cs
@@ -8,55 +8,73 @@ namespace CPS410Final
 {
     public class Database
     {
-        static string connectionstring = "Data Source=tutorsite.database.windows.net;Initial Catalog=siteDB;User ID=team10;Password=Cps410Cps410;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static SqlConnection connection = new SqlConnection("Data Source=tutorsite.database.windows.net;Initial Catalog=siteDB;User ID=team10;Password=Cps410Cps410;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
 
 
         /************** DATABASE OPERATIONS **************/
         static void openDB()
         {
-           // connection.Open();
+           connection.Open();
         }
 
         static void closeDB()
         {
-            //connection.Close();
+            connection.Close();
         }
 
 
         static Boolean nameExists(String username)
         {
-            SqlConnection connection = new SqlConnection(connectionstring);
             SqlDataReader reader;
             SqlCommand search = new SqlCommand("SELECT * FROM Users WHERE userName = @username", connection);
             search.Parameters.AddWithValue("@username", username);
 
-            connection.Open();
+            openDB();
 
             reader = search.ExecuteReader();
             reader.Read();
 
             if (reader.HasRows)
             {
-                
+                closeDB();
                 return true;
             }
             else
             {
                 // Username doesn't exist
+                closeDB();
                 return false;
             }
-
-
         }
 
-        public static Boolean addNewUser(String email, String username, String password)
+
+        public static String grabRole(String userID)
         {
-            SqlConnection connection = new SqlConnection(connectionstring);
+            SqlCommand search = new SqlCommand("SELECT * FROM Users WHERE UserID = @userID", connection);
+            search.Parameters.AddWithValue("@userID", userID);
+            SqlDataReader reader;
+            String role = "";
+
+            openDB();
+            reader = search.ExecuteReader();
+            reader.Read();
+
+            if (reader.HasRows)
+            {
+                role = reader["Role"].ToString();
+            }
+            closeDB();
+            return role;
+        }
+
+
+        public static String addNewUser(String email, String username, String password)
+        {
             if (nameExists(username))
             {
-                // Username is already found
-                return false;
+                // Username is already found, return an error message.
+                return "Username is taken. Please pick a new username.";
             }
             else
             {
@@ -64,30 +82,29 @@ namespace CPS410Final
                 SqlCommand insertUser = new SqlCommand("INSERT INTO Users (UserID, Username, UserPassword, UserSalt, UserEmail, TimeCreated) values (@userid, @username, @userpassword, @usersalt, @email, @time)", connection);
                 SqlCommand count = new SqlCommand("Select max(UserID) from Users", connection);
                 String userSalt;
-                // Should we have them confirm their password?
 
-                
-                // open database for interaction
-                connection.Open();
+
+                // Open database for interaction
+                openDB();
 
                 // Set the userID to be assigned
-
+                // Catches if the maxID number is nonexistent
                 int newuserID;
                 try
                 {
                     newuserID = (Int32)count.ExecuteScalar() + 1;
                 }
-                catch (InvalidCastException e)
+                catch (InvalidCastException)
                 {
                     newuserID = 0;
                 }
+
 
                 // Generate a salt for the new user and generate the hashed/salted password
                 userSalt = Security.genSalt();
                 String hashedPass = Security.Sha256((password + userSalt));
 
                 
-
                 // Assign parameters to SQL INSERT command
                 insertUser.Parameters.AddWithValue("@userid", newuserID);
                 insertUser.Parameters.AddWithValue("@username", username);
@@ -96,31 +113,106 @@ namespace CPS410Final
                 insertUser.Parameters.AddWithValue("@email", email);
                 insertUser.Parameters.AddWithValue("@time", DateTime.Now);
 
-                // Adds the user to the database. (OPTIONAL: Returns an int signifying how many rows were affected. Should only be 1?)
-                insertUser.ExecuteNonQuery();
 
-                connection.Close();
-                return true;
+                /*
+                 * Attempts to add the new user account to the database. If the addition is
+                 * successful, the method returns "TRUE". If the addition is unsuccessful
+                 * (in this case, for an error based on inserting to database), method returns
+                 * "FALSE".
+                 */
+                if (insertUser.ExecuteNonQuery() != 0)
+                {
+                    closeDB();
+                    return "TRUE";
+                }
+                else
+                {
+                    closeDB();
+                    return "FALSE";
+                }
             }
 
         }
+
+
+        public static Boolean addNewSubject(String UserID, String SubjName, Boolean chkbox) {
+
+            // Once created, take to a page OR Show something that will let the user create a new Subject
+            SqlCommand createSubject = new SqlCommand("INSERT INTO Subjects (SubjectID, SubjectName, SubjectCreator, SubjectVisibility) values(@SubjectID, @SubjectName, @SubjectCreator, @SubjectVisibility)", Database.connection);
+            SqlCommand count = new SqlCommand("Select max(SubjectID) from Subjects", Database.connection);
+            String SubjectID;
+
+
+            // Catches if the maxID number is nonexistent and assigns the SubjectID
+            int intID;
+            openDB();
+            try
+            {
+
+
+                // Try to find the maximum ID (highest ID number so far) and add one to it to make new SubjectID
+                intID = (Int32)count.ExecuteScalar() + 1;
+
+                SubjectID = intID.ToString();
+            }
+            catch (InvalidCastException)
+            {
+                int id = 0;
+                SubjectID = id.ToString();
+            }
+            closeDB();
+
+            // Assigning Subject visibility
+            String visible;
+            if (chkbox)
+            {
+                visible = "T";
+            }
+            else
+            {
+                visible = "F";
+            }
+
+
+            // Set INSERT parameters
+            createSubject.Parameters.AddWithValue("@SubjectID", SubjectID);
+            createSubject.Parameters.AddWithValue("@SubjectName", SubjName);
+            createSubject.Parameters.AddWithValue("@SubjectCreator", UserID);
+            createSubject.Parameters.AddWithValue("@SubjectVisibility", visible);
+
+
+            // Execute INSERT command
+            openDB();
+            if (createSubject.ExecuteNonQuery() == 0)
+            {
+                closeDB();
+                return true;
+            }
+            else
+            {
+                closeDB();
+                return false;
+            }
+            ;
+        }
+
 
         public static String validCredentials(String username, String password)
         {
             if (nameExists(username))
             {
-                SqlConnection connection = new SqlConnection(connectionstring);
                 SqlCommand grabInfo = new SqlCommand("SELECT * FROM Users WHERE Username = @username", connection);
                 SqlDataReader reader;
 
                 grabInfo.Parameters.AddWithValue("@username", username);
 
-                connection.Open();
+                openDB();
                 reader = grabInfo.ExecuteReader();
                 reader.Read();
 
                 if (!reader.HasRows)
                 { // No user found
+                    closeDB();
                     return "";
                 }
                 else
@@ -130,17 +222,21 @@ namespace CPS410Final
 
                     if (inputBased.Equals(reader["UserPassword"]))
                     { // Correct login information
-                        return reader["userID"].ToString();
+                        String userID = reader["userID"].ToString();
+                        closeDB();
+                        return userID;
                     }
                     else
                     { // Incorrect password
-                       return "";
+                        closeDB();
+                        return "";
                     }
                 }
             
             }
             else
             { // Not registered
+                closeDB();
                 return "";
             }
 
@@ -148,23 +244,6 @@ namespace CPS410Final
         }
 
         /************** END DATABASE OPERATIONS **************/
-
- /*       static SqlConnectionStringBuilder connectionBuilder()
-        {
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.ConnectionString = "Data Source = tutorsite.database.windows.net; Initial Catalog = siteDB; User ID = team10; " +
-                "Password = ********; Connect Timeout = 30; Encrypt = True; TrustServerCertificate = False; ApplicationIntent = ReadWrite;" +
-                " MultiSubnetFailover = False";
-            builder.DataSource = "tutorsite.database.windows.net";
-            builder.UserID = "team10";
-            builder.Password = "Cps410Cps410";
-            builder.InitialCatalog = "siteDB";
-
-            return builder;
-
-        } */
-
-
     }
 }
  
