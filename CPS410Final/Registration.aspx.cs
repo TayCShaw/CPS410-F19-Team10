3e690f173e50dd444a32e78ffebda5d4741588de
TayCShaw <47929530+TayCShaw@users.noreply.github.com>
2019-11-04 14:09:10 -0500

    Connection to database file.

--- CPS410Final/Registration.aspx.cs
+++ CPS410Final/Registration.aspx.cs
@@ -19,26 +19,29 @@ namespace CPS410Final
             if (txtboxPassword.Text.Equals(TextBoxConfirmPass.Text) && !txtboxEmail.Text.Equals("") && !txtboxUsername.Text.Equals(""))
             {
                 // TRUE if user was added, FALSE otherwise
-                bool userAdded = Database.addNewUser(txtboxEmail.Text, txtboxUsername.Text, txtboxPassword.Text);
+                String userAdded = Database.addNewUser(txtboxEmail.Text, txtboxUsername.Text, txtboxPassword.Text);
 
-                if (userAdded)
+                if (userAdded.Contains("TRUE"))
                 {
                     Response.Redirect("Account.aspx");
                 }
-                else
+                else if(userAdded.Contains("Username"))
                 {
-                    lblError.Text = "User Name is Unabailable";
+                    lblError.Text = userAdded;
+                }else if (userAdded.Contains("FALSE"))
+                {
+                    lblError.Text = "ERROR: Failed account creation. Please contact an Administrator.";
                 }
             }
             else
             {
                 if (txtboxEmail.Text.Equals(""))
                 {
-                    lblError.Text = "please enter a email";
+                    lblError.Text = "Please enter an email";
                 }
                 else if(txtboxUsername.Text.Equals(""))
                 {
-                    lblError.Text = "Please enter a User Name";
+                    lblError.Text = "Please enter a username";
                 }
                 else
                 {
