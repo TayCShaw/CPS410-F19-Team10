a422910b04a9f621a88032c258bb65842681bbe2
TayCShaw <47929530+TayCShaw@users.noreply.github.com>
2019-11-06 15:56:25 -0500

    yep

--- CPS410Final/Global.asax.cs
+++ CPS410Final/Global.asax.cs
@@ -10,7 +10,9 @@ namespace CPS410Final
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
-        {
+        { 
+
+
         }
     }
 }
