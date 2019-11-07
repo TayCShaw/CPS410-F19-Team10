f6f2324290592786c3a381224434037cfce9efda
Samuel Riegler <riegl2s@cmich.edu>
2019-11-07 16:50:39 -0500

    bullshit kinda works

--- CPS410Final/Subject.aspx.cs
+++ CPS410Final/Subject.aspx.cs
@@ -7,6 +7,13 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using static System.Drawing.ContentAlignment;
+
 
 /**
  * Page where you can pick the topic that wants to be explored.
@@ -18,57 +25,76 @@ namespace CPS410Final
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string getSubjects = "select * from Subjects";
-            string getTopics = "select * from Topics where Topics.TopicSubject = @subID";
-            SqlCommand subjectInformation = new SqlCommand(getSubjects, Database.connection);
-            SqlCommand topicsInformation = new SqlCommand(getTopics, Database.connection);
-            SqlDataReader topicsReader;
-            SqlDataReader subjectReader;
+            string oldTopic = "";
+
+            string getInfo = "Select Subjects.SubjectID, Subjects.SubjectName, Topics.TopicID, Topics.TopicName, " +
+                "Topics.TopicSubject from Subjects INNER JOIN Topics ON Subjects.SubjectID = Topics.TopicSubject ORDER BY Subjects.SubjectID ASC";
+
+            SqlCommand informationCommand = new SqlCommand(getInfo, Database.connection);
+            SqlDataReader getInformation;
 
             HtmlGenericControl mainDiv = new HtmlGenericControl("div");
             mainDiv.Attributes.Add("class", "mainDiv");
-          //  try
-          //  {
-                Database.openDB();
-                subjectReader = subjectInformation.ExecuteReader();
-                
-                // subject loop
-                while (subjectReader.Read())
-                {
-                    // get id and name here
-                    string subID = subjectReader["SubjectID"].ToString();
-                    string subName = subjectReader["SubjectName"].ToString();
-                    topicsInformation.Parameters.AddWithValue("@subID", subID);
-
-                    // reset the reader
-                    topicsReader = topicsInformation.ExecuteReader();
-
-                    //make a div for each subject
-                    HtmlGenericControl subjectDiv = new HtmlGenericControl("div");
-                    subjectDiv.Attributes.Add("class", "subjectDiv");
-                    mainDiv.Controls.Add(subjectDiv);
-                    
-                    // need to make subject header div here
-
-                    // while there are topics sill under the current subject add them to the current div
-                    while (topicsReader.Read())
-                    {
-                        // make a div for each topic
-                        HtmlGenericControl topicDiv = new HtmlGenericControl("div");
-                        topicDiv.Attributes.Add("class", "topics");
-                        subjectDiv.Controls.Add(topicDiv);
-                    }
-
-                    topicsReader.Close();
-
-                }
-           // }
-           // catch
-          //  {
-               // Form.Controls.Add(mainDiv);
-          //  }
             Form.Controls.Add(mainDiv);
 
+            HtmlGenericControl subjectDiv = new HtmlGenericControl("div");
+            HtmlGenericControl topicContainer = new HtmlGenericControl("div");
+
+            Database.openDB();
+            getInformation = informationCommand.ExecuteReader();
+
+            // subject loop
+            while (getInformation.Read())
+            {
+                // get id and name here
+                string subID = getInformation["SubjectID"].ToString();
+                string subName = getInformation["SubjectName"].ToString();
+                string topicName = getInformation["topicName"].ToString();
+
+                if (oldTopic.Equals(subID))
+                {
+                    // add this topic to the same topic div
+                    Label topicLbl = new Label();
+                    topicLbl.Text = topicName;
+                    topicLbl.CssClass = "label";
+                    HtmlGenericControl topicDiv = new HtmlGenericControl("div");
+                    topicDiv.Attributes.Add("class", "topicDiv");
+                    topicDiv.Attributes.Add("id", topicName);
+                    topicDiv.Controls.Add(topicLbl);
+                    topicContainer.Controls.Add(topicDiv);
+                }
+                else
+                {
+                    // we have moved on to a new subject so make a new subject div
+                    subjectDiv = new HtmlGenericControl("div");
+                    subjectDiv.Attributes.Add("class", "subjectDiv");
+                    subjectDiv.Attributes.Add("id", subName);
+
+                    Label subjectLbl = new Label();
+                    subjectLbl.Text = subName;
+                    subjectLbl.CssClass = "label";
+
+                    subjectDiv.Controls.Add(subjectLbl);
+                    mainDiv.Controls.Add(subjectDiv);
+
+                    topicContainer = new HtmlGenericControl("div");
+                    mainDiv.Controls.Add(topicContainer);
+
+                    Label topicLbl = new Label();
+                    topicLbl.Text = topicName;
+                    topicLbl.CssClass = "label";
+                    HtmlGenericControl topicDiv = new HtmlGenericControl("div");
+                    topicDiv.Attributes.Add("class", "topicDiv");
+                    topicDiv.Attributes.Add("id", topicName);
+                    topicDiv.Controls.Add(topicLbl);
+                    topicContainer.Controls.Add(topicDiv);
+                    topicContainer.Attributes.Add("class", "topicContainer");
+
+                    oldTopic = subID;
+
+                }
+            }
+            Database.closeDB();
         }
 
 
